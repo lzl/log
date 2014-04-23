520fb62bbf24654e8d4aa11e958a8a67e9f90307
lizunlong <lizunlong@gmail.com>
2014-04-23 17:32:07 +0800

    Counts is almost done.

--- main.js
+++ main.js
@@ -257,6 +257,25 @@ if (Meteor.isClient) {
     t.style.height = 'auto';
     t.style.height = (t.scrollHeight  + offset + 27) + 'px';
   }
+
+  ///// Counts /////
+  // via https://atmospherejs.com/package/publish-counts
+  Counts = new Meteor.Collection('counts');
+
+  Counts.get = function(name) {
+    var count = Counts.findOne(name);
+    return count && count.count;
+  };
+
+  Meteor.subscribe("userCounts");
+  Meteor.subscribe("logCounts");
+
+  UI.body.userCounts = function () {
+    return Counts.get("userCounts");
+  };
+  UI.body.logCounts = function () {
+    return Counts.get("logCounts");
+  };
 }
 
 if (Meteor.isServer) {
@@ -281,5 +300,41 @@ if (Meteor.isServer) {
     remove: function (userId, doc) {
       return doc.user_id === userId;
     }
+  });
+
+  ///// Counts /////
+  // via https://atmospherejs.com/package/publish-counts
+  publishCount = function(self, name, cursor, options) {
+    var count = 0;
+    var initializing = true;
+    options = options || {}
+
+    var handle = cursor.observeChanges({
+      added: function(id) {
+        count += 1;
+        if (! initializing)
+          self.changed('counts', name, { count: count });
+      },
+      removed: function(id) {
+        count -= 1;
+        self.changed('counts', name, { count: count });
+      }
+    });
+
+    initializing = false;
+    self.added('counts', name, { count: count });
+    if (! options.noReady) self.ready();
+
+    self.onStop(function() {
+      handle.stop();
+    });
+  };
+
+  Meteor.publish('userCounts', function() {
+    publishCount(this, 'userCounts', Meteor.users.find());
+  });
+
+  Meteor.publish('logCounts', function() {
+    publishCount(this, 'logCounts', Logs.find());
   });
 }
